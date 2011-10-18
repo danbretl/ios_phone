706863c68e01f6eabd23da282e3c176476385cf0
Dan Bretl <danbretl@gmail.com>
2011-10-18 11:34:30 -0400

    Progress on location setter views

--- kwiqet/classes/LocationView.h
+++ kwiqet/classes/LocationView.h
@@ -5,9 +5,12 @@
 //  Created by Dan Bretl on 10/18/11.
 //  Copyright (c) 2011 Abextra Technologies. All rights reserved.
 //
+//  A LocationView will not display its locationLabel properly if it is square. Ideally, it is meant to be vertically oriented.
 
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
+
+//extern NSString * const LOCATION_VIEW_SPAN_SMALL;
 
 @interface LocationView : UIView {
     
@@ -15,5 +18,10 @@
     UILabel * locationLabel;
     
 }
+
+@property (retain) MKMapView * mapView;
+@property (retain) UILabel * locationLabel;
+
+- (void) setLocation:(CLLocation *)location withLocationString:(NSString *)locationString;
 
 @end
--- kwiqet/classes/LocationView.m
+++ kwiqet/classes/LocationView.m
@@ -7,16 +7,66 @@
 //
 
 #import "LocationView.h"
+#import "UIFont+Kwiqet.h"
+
+//NSString * const LOCATION_VIEW_SPAN_SMALL = @"LOCATION_VIEW_SPAN_SMALL";
+
+float const LOCATION_VIEW_INNER_PADDING = 5.0;
+float const LOCATION_VIEW_MAP_LABEL_SPACING = 5.0;
+
+@interface LocationView(Private)
+// ...
+@end
 
 @implementation LocationView
+
+@synthesize mapView, locationLabel;
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
+        
+        // Allocating & initializing
+        // Map view
+        mapView = [[MKMapView alloc] init];
+        self.mapView.scrollEnabled = NO;
+        self.mapView.zoomEnabled = NO;
+        [self addSubview:self.mapView];
+        // Location label
+        locationLabel = [[UILabel alloc] init];
+        self.locationLabel.numberOfLines = 0;
+        [self addSubview:self.locationLabel];
+        
+        // Positioning & autoresizing
+        CGFloat mapViewSideLength = MIN(frame.size.width, frame.size.height) - 2 * LOCATION_VIEW_INNER_PADDING;
+        self.mapView.frame = CGRectMake(LOCATION_VIEW_INNER_PADDING, LOCATION_VIEW_INNER_PADDING, mapViewSideLength, mapViewSideLength);
+        if (frame.size.width < frame.size.height) {
+            CGFloat locationLabelOriginY = CGRectGetMaxY(self.mapView.frame) + LOCATION_VIEW_MAP_LABEL_SPACING;
+            self.locationLabel.frame = CGRectMake(LOCATION_VIEW_INNER_PADDING, locationLabelOriginY, self.mapView.frame.size.width, frame.size.height - LOCATION_VIEW_INNER_PADDING - locationLabelOriginY);
+            self.mapView.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin;
+            self.locationLabel.autoresizingMask = UIViewAutoresizingFlexibleHeight;
+        } else {
+            CGFloat locationLabelOriginX = CGRectGetMaxX(self.mapView.frame) + LOCATION_VIEW_MAP_LABEL_SPACING;
+            self.locationLabel.frame = CGRectMake(locationLabelOriginX, LOCATION_VIEW_INNER_PADDING, frame.size.width - LOCATION_VIEW_INNER_PADDING - locationLabelOriginX, self.mapView.frame.size.height);
+            self.mapView.autoresizingMask = UIViewAutoresizingFlexibleRightMargin;
+            self.locationLabel.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+        }
     }
     return self;
+}
+
+- (void)setLocation:(CLLocation *)location withLocationString:(NSString *)locationString {
+    // ...
+    // ...
+    // ...
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+    [mapView release];
+    [locationLabel release];
 }
 
 /*
