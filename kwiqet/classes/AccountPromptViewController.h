41aa28e7b54cb1178910dd60285e4a73de262fa3
Dan Bretl <danbretl@gmail.com>
2011-11-28 19:48:40 -0500

    Prepping for account registration / login revamp

---
+++ kwiqet/classes/AccountPromptViewController.h
@@ -0,0 +1,13 @@
+//
+//  AccountPromptViewController.h
+//  kwiqet
+//
+//  Created by Dan Bretl on 11/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountPromptViewController : UIViewController
+
+@end
